51d5ebf0b893664873d8601f6c076dc91bc7a06e
aman <amansagar262@gmail.com>
2023-12-10 04:35:51 +0530

    chore: update api

--- Backend/packages/api/index.ts
+++ Backend/packages/api/index.ts
@@ -1,5 +1,6 @@
-import cors from "cors";
-import express from "express";
+import * as cors from "cors";
+import * as express from "express";
+
 import { type FormData } from "../deployer/src/types/FormData";
 import { runDeployer } from "../deployer/src/runDeployer";
 
@@ -9,12 +10,13 @@ const port = 8000;
 app.use(express.json());
 app.use(cors());
 
-app.post("/deploy", (req, res) => {
+app.post("/deploy", async (req, res) => {
   const body = req.body as FormData;
   try {
-    runDeployer(body);
+    await runDeployer(body);
     return res.status(200).json({ message: "Deployed successfully" });
   } catch (error) {
+    console.log(error);
     return res.status(500).json({ message: "Something went wrong" });
   }
 });
